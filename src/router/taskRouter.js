781c3bf87d7f91c151fecf713961afc1760ac9f3
Harwin <sandhuharwinkaur@gmail.com>
2026-04-02 22:05:06 +1100

    added

--- src/router/taskRouter.js
+++ src/router/taskRouter.js
@@ -1,6 +1,44 @@
 import express from 'express'
 const router = express.Router()
 
+// router.all("/", (req, res, next) => {
+//     // do your code here
 
+//     // res.json({
+//     //     status: "success",
+//     //     message: "response from all",
+//     // });
+
+//     next();
+// });
+
+router.post("/", (req, res, next) => {
+    // do your code here
+    res.json({
+        status: "success",
+        message: "response from post",
+    });
+});
+router.get("/", (req, res, next) => {
+    // do your code here
+    res.json({
+        status: "success",
+        message: "response from get",
+    });
+});
+router.put("/", (req, res, next) => {
+    // do your code here
+    res.json({
+        status: "success",
+        message: "response from put",
+    });
+});
+router.delete("/", (req, res, next) => {
+    // do your code here
+    res.json({
+        status: "success",
+        message: "response from delete",
+    });
+});
 
 export default router
